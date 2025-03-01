ccd0a6f2525f4c7a5cbd9cd0d610c4ba507a24e1
Nadim-2003 <168224356+Nadim-2003@users.noreply.github.com>
2025-03-01 09:50:44 +0600

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -6,13 +6,13 @@ struct node
 {
   int val;
   node *left;
-  node *mid;
+  //node *mid;
   node *right;
   node(int n)
   {
     val = n;
     left = nullptr;
-    mid = nullptr;
+    //mid = nullptr;
     right = nullptr;
   }
 };
@@ -25,9 +25,9 @@ node *findkey(node *root, int k)
   node *foundnode = findkey(root->left, k);
   if (foundnode)
     return foundnode;
-   foundnode = findkey(root->mid, k);
-  if (foundnode)
-     return foundnode;
+  // foundnode = findkey(root->mid, k);
+  // if (foundnode)
+  //   return foundnode;
   return findkey(root->right, k);
 }
 void import(node* root)
@@ -48,21 +48,29 @@ void import(node* root)
       }
       if (op == 1)
       {
-        temp->mid = mover;
+        temp->right = mover;
       }
-       if (op == 2)
-      {
-         temp->right = mover;
-       }
+      // if (op == 2)
+      // {
+      //   temp->right = mover;
+      // }
     }
   }
 }
+int isSameTree(node* p,node* q) {
+  
+    if (p == nullptr || q == nullptr) {
+        return p == q ? 1 : 0; 
+    }
+    return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right) ? 1 : 0;
+}
+
 void traversal(node *root)
 {
   if (!root)
     return;
   traversal(root->left);
-  traversal(root->mid);
+  // traversal(root->mid);
   cout << root->val << endl;
   traversal(root->right);
 }
@@ -72,6 +80,11 @@ int main()
   cin >> r;
   node *root1 = new node(r);
   import(root1);
-   traversal(root1);
+  int r1;
+  cin>>r1;
+  node* root2=new node(r1);
+  import(root2);
+  int ok=isSameTree(root1,root2);
+  cout<<ok<<endl;
   return 0;
 }
